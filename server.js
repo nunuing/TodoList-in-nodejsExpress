ac72a0f36e40f8b92754ccbe42942d07d34fddad
eycho <chw3872@gmail.com>
2023-06-30 12:16:48 +0900

    html file send

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express');         //package 불러옴
+const app = express();                      // express 함수의 변환값 저장
+const port = process.env.PORT || 3000;      //process.env = 환경변수 호출, 환경변수가 입력되지 않을 시 port에 3000번을 지정
 
+//get 정의
 app.get('/', (req, res) => {
-    res.json({
-        success: true,
-    });
+    res.sendFile(__dirname + "/static/home.html");
+    console.log(__dirname);
 });
 
 app.listen(port, () => {
